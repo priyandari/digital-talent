a69af4ebf9740b6004f3fac9b303537db8aaf537
Priyandari <pri.and.ari@gmail.com>
2018-10-26 07:23:34 +0700

    Update file dari local

---
+++ data-sciencetutorial.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 24 10:12:10 2018
+
+@author: Yusuf
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+data.Speed.plot(kind='line', color='g', label='speed', linewidth=1, alpha=0.5, grid = True, linestyle =':')
